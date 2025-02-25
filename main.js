1ca3c5ac12f5fe65c1bcf3603bd5231998b2f91e
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2025-02-25 15:00:36 -0400

    Update main.js
    Include findHighPopulationCountries function in main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@
 
 // MARKING
 // 10/10 - Function is completed to spec and submitted within class time
-//  8/10 - Function is completed to spec and submitted within grace period of twelve hours of end of class time
-//  6/10 - Function is completed to spec and submitted after twelve-hour grace period
+//  8/10 - Function is completed to spec and submitted within a grace period of twelve hours of the end of class time
+//  6/10 - Function is completed to spec and submitted after a twelve-hour grace period
 //  0/10 - Function is not completed
 
 
@@ -20,8 +20,11 @@
         .then(response => response.json())
         .then(json => {
             //DO NOT MODIFY THIS CODE
-            document.write(`The total population of countries that border China and list English as an official language is ${getPopulation(json)}`)
-            test.run(getPopulation(json)) // will output test result to browser console
+            const populationResult = getPopulation(json);
+            const countriesResult = findHighPopulationCountries(json);
+            document.write(`<h3>The total population of countries that border China and list English as an official language is ${populationResult}</h3>`)
+            document.write(`<h3>Countries in Europe with a population greater than 85 million: ${countriesResult.join(', ')}</h3>`)            
+            test.run(populationResult, countriesResult) // will output test result to browser console
         })
 
     // WRITE YOUR getPopulation FUNCTION BELOW
