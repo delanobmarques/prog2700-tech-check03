66d39c35a8139a5aa410898ea3c0c3d2746d2d10
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2025-02-25 15:02:08 -0400

    Update test.js - include findHighPopulationCountries function in tests

--- test.js
+++ test.js
@@ -1,20 +1,36 @@
-//DO NOT MODIFY THIS CODE IN ANY WAY....USED TO TEST YOUR getPopulation FUNCTION
+//DO NOT MODIFY THIS CODE IN ANY WAY....USED TO TEST YOUR getPopulation and findHighPopulationCountries FUNCTIONS
 const test = {
     fontSize: '20px',
     passColor: '#00FF00',
     failColor: '#FF0000',
-    correctOutput: 1608378516,
-    run: function(input) {
-        const displayInput = input === '' ? 'an Empty String' : input
-        if(input === this.correctOutput) {
+    correctOutputPopulation: 1608378516,
+    correctOutputCountries: ["Russian Federation"], // Example expected output
+    run: function(populationInput, countriesInput) {
+        const displayPopulationInput = populationInput === '' ? 'an Empty String' : populationInput;
+        const displayCountriesInput = Array.isArray(countriesInput) && countriesInput.length > 0 ? countriesInput.join(', ') : 'an Empty Array or Undefined';
+
+        if(populationInput === this.correctOutputPopulation) {
             console.log(
                 `%cFUNCTION getPopulation is CORRECT`, 
                 `color: ${this.passColor}; font-size: ${this.fontSize};`
-            )
+            );
         } else {
             console.log(
-                `%cFUNCTION getPopulation is INCORRECT\n\tExpected ${this.correctOutput} but got ${displayInput}`,
-                `color: ${this.failColor}; font-size: ${this.fontSize};`)
+                `%cFUNCTION getPopulation is INCORRECT\n\tExpected ${this.correctOutputPopulation} but got ${displayPopulationInput}`,
+                `color: ${this.failColor}; font-size: ${this.fontSize};`
+            );
+        }
+
+        if(Array.isArray(countriesInput) && JSON.stringify(countriesInput) === JSON.stringify(this.correctOutputCountries)) {
+            console.log(
+                `%cFUNCTION findHighPopulationCountries is CORRECT`, 
+                `color: ${this.passColor}; font-size: ${this.fontSize};`
+            );
+        } else {
+            console.log(
+                `%cFUNCTION findHighPopulationCountries is INCORRECT\n\tExpected ${this.correctOutputCountries.join(', ')} but got ${displayCountriesInput}`,
+                `color: ${this.failColor}; font-size: ${this.fontSize};`
+            );
         }
     }
 }
