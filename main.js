34023595a75b2a4f607d7f0037b9cb5b414681ef
Delano Marques <54899930+delanobmarques@users.noreply.github.com>
2025-11-10 16:42:11 -0400

    Refactor DOM manipulation and data handling

--- main.js
+++ main.js
@@ -15,13 +15,18 @@
         .then(response => response.json())
         .then(json => {
             //DO NOT MODIFY THIS CODE
-            const populationResult = getPopulation(json);
-            const countriesResult = findHighPopulationCountries(json);
-            document.write(`<h3>The total population of countries that border China and list English as an official language is ${populationResult}</h3>`)
-            document.write(`<h3>Countries in Europe with a population greater than 85 million: ${countriesResult.join(', ')}</h3>`)            
+            const populationResult = getPopulation(json.data);
+            const countriesResult = findHighPopulationCountries(json.data);            
+            const h3Population = document.createElement('h3');
+            h3Population.textContent = `The total population of countries that border China and list English as an official language is ${populationResult}`;
+            document.body.appendChild(h3Population);            
+            const h3Countries = document.createElement('h3');
+            h3Countries.textContent = `Countries in Europe with a population greater than 85 million: ${countriesResult.join(', ')}`;
+            document.body.appendChild(h3Countries);
+            
             test.run(populationResult, countriesResult) // will output test result to browser console
         })
-
+    
     // WRITE YOUR getPopulation and findHighPopulationCountries FUNCTIONS BELOW
     // |           |             |           |
     // V           V             V           V
@@ -30,4 +35,5 @@
     
 
 })(test);
+
 
